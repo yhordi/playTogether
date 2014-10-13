3dbdc4e321e285d75bff4e7c91a4d799f321cae3
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-10-13 12:44:44 -0700

    ajax working, sinatra working

--- public/js/script.js
+++ public/js/script.js
@@ -9,31 +9,6 @@ var Steam = function(){
 }
 
 Steam.prototype = {
-  // ajaxRequest: function(e){
-  //   var here = this
-  //   $.ajax({
-  //     dataType: 'Json',
-  //     type: 'get',
-  //     url: "/HTTParty",
-  //     data: {
-  //       key: 'E6B0ADB9B2C8C41CC3A0D86C3F235406',
-  //       steamids: '76561197985852059',
-  //     },
-  //     success: function (data) {
-  //       ajaxData = data
-  //       console.log(data);
-  //       console.log(here)
-  //       here.steam.injectResponse(ajaxData)
-  //     },
-  //     error: function (bug) {
-  //       console.log(bug)
-  //     },
-  //     complete:function (data) {
-  //       var status = data.status
-  //       response = $.parseJSON(data.responseText);
-  //     }
-  //   })
-  // },
   getInfo: function(){
     $.ajax({
       dataType: 'json',
