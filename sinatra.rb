ecbbaeb8e9562064295bc6960620776776acd5e8
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-10-13 15:05:51 -0700

    created 'sign in with steam button'

--- sinatra.rb
+++ sinatra.rb
@@ -2,13 +2,20 @@ require 'sinatra'
 require 'httparty'
 require 'dotenv'
 Dotenv.load(File.expand_path("../.env",  __FILE__))
+require "omniauth/strategies/steam"
+
 
 get '/' do
   redirect 'index.html'
 end
-get '/HTTParty' do
 
+get '/HTTParty' do
   @party = HTTParty.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{ENV['SECRET_KEY']}&steamids=76561197985852059")
-  p @party
   @party.to_json
+
+post "/auth/steam/callback" do
+  content_type "text/plain"
+  request.env["omniauth.auth"].info.to_hash.inspect
+end
+
 end
